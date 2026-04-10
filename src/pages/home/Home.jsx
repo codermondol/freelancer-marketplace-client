912581e1a463af9dd155ec378d4a192dde2e46e6
my first github repo <mrarifmondol98@gmail.com>
2026-04-10 11:53:51 -0800

    added banned design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Banner from './HomePage/Banner';
+import React from "react";
+import Banner from "./HomePage/Banner";
+import Categories from "./HomePage/Categories";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Categories></Categories>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/home/HomePage/Banner.jsx
+++ src/pages/home/HomePage/Banner.jsx
@@ -3,18 +3,20 @@ import heroBg from "./../../../assets/bg-hero.png";
 
 const Banner = () => {
   return (
-    <div className="w-10/12 mx-auto flex flex-col lg:flex-row justify-between items-center gap-10 py-4 lg:py-20">
-      <div className="space-y-10 text-center lg:text-left">
-        <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold">
-          The Easiest Way to Get Your New Job
-        </h1>
-        <p className="text-base sm:text-lg lg:text-xl text-gray-600">
-          Work with talented people at the most affordable price to get the most
-          out of your time and cost
-        </p>
-      </div>
-      <div className="w-full lg:w-1/2">
-        <img src={heroBg} alt="Hero" className="w-full h-auto" />
+    <div className="bg-secondary">
+      <div className="w-10/12 mx-auto flex flex-col lg:flex-row justify-between items-center gap-10 py-4 lg:py-20">
+        <div className="space-y-10 text-center lg:text-left">
+          <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold text-white">
+            The Easiest Way to Get Your New Job
+          </h1>
+          <p className="text-base sm:text-lg lg:text-xl text-gray-300">
+            Work with talented people at the most affordable price to get the
+            most out of your time and cost
+          </p>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <img src={heroBg} alt="Hero" className="w-full h-auto" />
+        </div>
       </div>
     </div>
   );
